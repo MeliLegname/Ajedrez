f8013a26126411922ebfe5c843d6041d3ea5f174
Melisa <mlegname@moorea.io>
2019-06-14 16:02:23 -0300

    Ramdom Move

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Board from './components/Board';
+import { observe } from './components/Game';
 
-ReactDOM.render(<Board knightPosition={[ 0, 0 ]} />, document.getElementById('root'));
+const root = document.getElementById('root');
+
+observe((knightPosition) => ReactDOM.render(<Board knightPosition={knightPosition} />, root));
